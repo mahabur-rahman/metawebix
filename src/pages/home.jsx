b640449e0e5934f16a1dd6d95ee36618bc847edd
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-09 10:44:03 +0600

    tree chart

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import MyChart from "../components/MyChart";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Topbar from "../components/Topbar/Topbar";
 import VideoCard from "../components/mainVideoCards/VideoCard";
@@ -5,11 +6,13 @@ import VideoCard from "../components/mainVideoCards/VideoCard";
 const Home = () => {
   return (
     <>
-      <Topbar title="YouTube" />
+      {/* <Topbar title="YouTube" />
       <div className="d-flex">
         <Sidebar />
         <VideoCard />
-      </div>
+      </div> */}
+
+      <MyChart />
     </>
   );
 };
