056412c670052ce00c9813d3c6b94bab46cb3675
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-22 19:10:55 +0600

    register use successful!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-// global  styles
-import Gmeet from "./components/gmeet";
-import "./global.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Gmeet />
+      <Header />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
     </>
   );
 };
---
+++ src/components/Login.jsx
@@ -0,0 +1,40 @@
+import { Button, Col, Form, Row } from "react-bootstrap";
+
+const Login = () => {
+  return (
+    <>
+      <Form style={{ width: "60%", margin: "0 auto" }}>
+        <h2 className="text-center py-4">Sign In </h2>
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" placeholder="Email" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group
+          as={Row}
+          className="mb-3"
+          controlId="formHorizontalPassword"
+        >
+          <Form.Label column sm={2}>
+            Password
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit">Sign in</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </>
+  );
+};
+
+export default Login;
