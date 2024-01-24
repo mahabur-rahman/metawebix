78cf6ea9c05e755cb2a2d6b2b82bc226014a9bb1
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-24 18:14:32 +0600

    rtk added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
-import { useState, useEffect } from "react";
 import { Container, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom"; // Correct import statement
 import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setAuth } from "../redux/authSlice";
 
 function Header() {
-  const [auth, setAuth] = useState(false);
+  const auth = useSelector((state) => state.auth.value);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async () => {
-      await axios.get(`/logout`);
-      setAuth(true);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       await axios.get(`/logout`);
+  //       setAuth(true);
+  //     } catch (err) {
+  //       setAuth(false);
+  //     }
+  //   })();
+  // }, []);
 
   const logout = async () => {
     await axios.post("/logout", {}, { withCredentials: true });
+    dispatch(setAuth(false));
     // every headers set token ''
     axios.defaults.headers.common["Authorization"] = ``;
   };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,33 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuth } from "../redux/authSlice";
 
 const Home = () => {
-  const [message, setMessage] = useState("You are not logged in");
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth.value);
+
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     (async () => {
-      const { data } = await axios.get(`/me`);
-      setMessage(`${data.firstName} - ${data.lastName}`);
+      try {
+        const { data } = await axios.get(`/me`);
+        setMessage(`${data.firstName} - ${data.lastName}`);
+
+        dispatch(setAuth(true));
+      } catch (err) {
+        console.log(err.message);
+        dispatch(setAuth(false));
+      }
     })();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
       <h1 className="text-center">Home Page</h1>
 
-      <h3>{message}</h3>
+      <h3>{auth ? message : "You are not authenticated!"}</h3>
     </div>
   );
 };
