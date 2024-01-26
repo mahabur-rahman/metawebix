f346c7eeaa2f6f80a4e9db9f7bfa42f3f790c9d3
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-26 15:35:02 +0600

    attach analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-1503LXE8MQ"); // measurement ID
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
